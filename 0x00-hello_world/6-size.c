edc1638d60b3374c6dd36c1b4320ea98fcf368ce
Adegoke <adelakunbamidele@gmail.com>
2023-02-19 13:16:52 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'program prints size of  various types'
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	/*print size of char*/
+	printf("Size of char: %ld byte\n", sizeof(char));
+	/*print size of int*/
+	printf("Size of int: %ld byte\n", sizeof(int));
+	/*print size of long*/
+	printf("Size of long: %ld byte\n", sizeof(long));
+	/*print size of long long int*/
+	printf("Size of long int: %ld byte\n", sizeof(long long int));
+	/*print size of float*/
+	printf("Size of float: %ld byte\n", sizeof(float));
+	return (0);
+}
