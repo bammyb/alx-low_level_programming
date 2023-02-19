9b5e3e10ad62915a6d9d98b02150711b3a25bc3e
Adegoke <adelakunbamidele@gmail.com>
2023-02-19 12:35:18 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'program print text to screen with print f'
+ *
+ * Return: Always 0 Success
+ */
+
+int main(void)
+{
+	/*print to screen with printf fn*/
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
