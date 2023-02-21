51c2d85d92642ba63cc7848d95f2a39c65e28a14
Adegoke <adelakunbamidele@gmail.com>
2023-02-20 19:38:56 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int base = 10;
+	int n = 0;
+
+	for (; n < base; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+
+	return (0);
+}
