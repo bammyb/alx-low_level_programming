a4f5938f3e606238a16b5ddc40dfd51930b6540c
Adegoke <adelakunbamidele@gmail.com>
2023-02-20 21:46:38 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (j = 97; j < 103; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
