3588b27449bd0a3f05499b964767c12a4c7503e4
Adegoke <adelakunbamidele@gmail.com>
2023-02-22 20:25:26 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ *Check if a alphabet is lowercasr
+ *
+ * Return: if it is lowercase return 1 else 0
+ */
+
+int _islower(int c)
+{
+	if ( c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
