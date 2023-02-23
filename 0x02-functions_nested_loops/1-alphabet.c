6ddd65233227d7e853df236d60f4e7d53f80476e
Adegoke <adelakunbamidele@gmail.com>
2023-02-22 17:14:41 -0800

    adding project 1 & 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Description:
+ */
+
+void print_alphabet(void)
+{
+	char al = 'a';
+
+	while (al < 'z')
+	{
+		_putchar(al);
+		al++;
+	}
+	_putchar('\n');
+}
