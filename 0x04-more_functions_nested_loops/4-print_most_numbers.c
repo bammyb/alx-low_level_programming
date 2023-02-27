46e1dd497ee2891cefbc642bb3f3561272d75f72
Adegoke <adelakunbamidele@gmail.com>
2023-02-27 15:45:36 -0800

    adding proj 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_most_numbers(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		if (i == '2' || i == '4')
+		{
+			
+		}
+		else
+		{
+			_putchar(i);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
