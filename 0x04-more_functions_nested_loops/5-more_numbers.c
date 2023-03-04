b61a2d8a5592972c1af18d0c60fa3f87fbd63570
Adegoke <adelakunbamidele@gmail.com>
2023-03-04 15:57:14 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,13 @@
 
 void more_numbers(void)
 {
-	int d,i;
+	int d, i;
 
 	for (d = 0; d < 10; d++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			if( i > 9)
+			if (i > 9)
 			{
 				_putchar((i / 10) + '0');
 			}
