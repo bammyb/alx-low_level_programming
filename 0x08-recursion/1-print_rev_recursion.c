0e176e29e178bebf6533332f2cc2931901b7d9ea
Adegoke <adelakunbamidele@gmail.com>
2023-03-08 20:47:29 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,18 +5,14 @@
  *
  * @s:
  *
- * Return: result len
+ * Return: void
  */
 
-int _strlen_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-	int len = 0;
-
 	if (*s)
 	{
-		len++;
-		len += _strlen_recursion(s + 1);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-
-	return (len);
 }
